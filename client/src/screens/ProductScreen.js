e12816893d2f9e00393c354d89a71ce318e6457a
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2023-05-18 01:42:11 +0330

    Homescreen done

---
+++ client/src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
+
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id); //match to access id in the url
+  return <div>{product.name}</div>;
+};
+
+export default ProductScreen;
