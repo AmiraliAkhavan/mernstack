d7070f35dbe204e8aa835d01ea1b69815ba10597
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2023-05-18 13:30:37 +0330

    setting express and axios

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,12 +1,96 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  ListGroupItem,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id); //match to access id in the url
-  return <div>{product.name}</div>;
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const res = await axios.get(`/api/products/${match.params.id}`);
+
+      setProduct(res.data);
+    };
+    fetchProduct();
+  }, []);
+
+  return (
+    <>
+      <Link className="btn btn-dark my-3 rounded" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image
+            fluid
+            rounded
+            src={product.image}
+            alt={product.name}
+            style={{ border: "1px solid black" }}
+          />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h3>{product.name}</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} Reviews`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>Price: {product.price}</ListGroupItem>
+            <ListGroupItem>
+              Product description: {product.description}
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card className="rounded">
+            <ListGroup>
+              <ListGroupItem>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>{product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button
+                  className="btn-block w-100 bg-success"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
